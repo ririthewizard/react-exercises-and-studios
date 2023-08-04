7b69c27bdff359529672d617b1485cf1d8104699
Riley <ririwilliams.rw@gmail.com>
2023-08-03 20:29:47 -0500

    Created a React app for a Pho recipe

---
+++ part1/studio/src/components/Ingredients.js
@@ -0,0 +1,28 @@
+import React from "react";
+import styles from "./Ingredients.module.css";
+
+function RecipeIngredients(){
+    const ingredients = [
+        "Bones & beef",
+        "Onion",
+        "Ginger",
+        "Spices",
+        "Rice noodles"
+    ];
+
+    return (
+        <div>
+        <h3>Recipe Ingredients</h3>
+        <ul className = {styles.ingredientList}>
+           <li>{ingredients[0]}</li>
+           <li>{ingredients[1]}</li>
+           <li>{ingredients[2]}</li>
+           <li>{ingredients[3]}</li>
+           <li>{ingredients[4]}</li>
+        </ul>
+     </div>
+  
+    )
+}
+
+export default RecipeIngredients;
