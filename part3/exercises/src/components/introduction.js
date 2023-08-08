04f9891c626868760f7122b090141350c9093123
Riley <ririwilliams.rw@gmail.com>
2023-08-08 13:29:39 -0500

    Added exercises dir for p3 and created HobbyIntroduction component

---
+++ part3/exercises/src/components/introduction.js
@@ -0,0 +1,14 @@
+function HobbyIntroduction(){
+    return(
+        <div>
+            <h2>3 Rad Facts About Skateboarding</h2>
+            <ol>
+                <li>It's great exercise</li>
+                <li>It is a fantastic way to be creative</li>
+                <li>You can even move around the city on your skateboard!</li>
+            </ol>
+        </div>
+    );
+}
+
+export default HobbyIntroduction;
