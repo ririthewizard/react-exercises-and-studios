996f03aba279da183031212bdc3ee4b1d91b9729
Riley <ririwilliams.rw@gmail.com>
2023-08-08 14:03:47 -0500

    Added a project component that displays the different trails around KC with some info and a picture. Also handles onClick

---
+++ part3/exercises/src/components/Projects.js
@@ -0,0 +1,33 @@
+import data from "./../data.json";
+import { useState } from "react";
+
+function MyProjects() {
+    const [index, setIndex] = useState(0);
+
+    let pushes = data.pushes;
+    let push = pushes[index];
+
+    function handleClick() {
+        if (index < data.pushes.length -1){
+            setIndex(index+1);
+        } else {
+            setIndex(0)
+        }
+    }
+
+    return (
+        <div>
+            <button onClick={handleClick}>
+                Next
+            </button>
+            <h2>{push.trail} at {push.trailHead}</h2>
+                <p>The {push.trail} is {push.trailLengthOneWay} long</p>
+                <img src={push.photoUrl} />
+        </div>
+    )
+
+}
+
+
+
+
